44d46bf7a935e49e88a274710215cd745c2e734a
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-23 12:46:06 +0100

    feat:create roleController & crud deleteRoles, updateRoles, createdRoles

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -6,3 +6,23 @@ export const getRoles = (req:Request, res:Response) => {
     message: "Roles retrieved successfuly",
   })
 }
+
+export const createdRoles = (req:Request, res:Response) => {
+    res.status(200).json({
+        success: true,
+        message: "Roles Created  successfuly",
+    })
+}
+export const updatedRoles = (req:Request, res:Response) => {
+    res.status(200).json({
+        success: true,
+        message: "Roles Updated  successfuly",
+    })
+}
+
+export const deletedRoles = (req:Request, res:Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Delete roles",
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Application } from "express"
-import { getRoles } from "./controllers/roleController"
+import { createdRoles, deletedRoles, getRoles, updatedRoles } from "./controllers/roleController"
 
 import dotenv from "dotenv"
 
@@ -17,6 +17,12 @@ app.get("/healthy", (req, res) => {
 })
 // creo el endpoint roles.
 app.get("/roles",getRoles)
+
+app.post("/roles",createdRoles)
+
+app.put("/roles",updatedRoles)
+
+app.delete("/roles",deletedRoles)
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT: ${PORT}`)
