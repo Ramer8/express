1a1091915d118028a4caa0251890dbbd12502a6d
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-24 22:37:48 +0100

    used params to get id & body to get json object inserted with thunder Client

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -8,12 +8,29 @@ export const getRoles = (req:Request, res:Response) => {
 }
 
 export const createdRoles = (req:Request, res:Response) => {
+
+    // Recuperar la info a traves del body
+
+    // console.log(req.body);
+
+    // const name = req.body.tasks
+
+    const { task } = req.body.tasks[1] //hacemos destructuring y me traigo el id 
+
+
+    console.log(task);
     res.status(200).json({
         success: true,
         message: "Roles Created  successfuly",
     })
 }
 export const updatedRoles = (req:Request, res:Response) => {
+
+//recuperar parametro (params) de la ruta
+req.params.id
+
+console.log(req.params.id);
+
     res.status(200).json({
         success: true,
         message: "Roles Updated  successfuly",
@@ -21,6 +38,9 @@ export const updatedRoles = (req:Request, res:Response) => {
 }
 
 export const deletedRoles = (req:Request, res:Response) => {
+//recuperar parametro de la ruta
+req.params.id
+console.log(req.params.id)
   res.status(200).json({
     success: true,
     message: "Delete roles",
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,11 @@ dotenv.config()
 
 const app: Application = express()
 
+// Cdo mi app esta en marcha va leer de arriba abajo, lo q nos llegue lo va a pasar a json hasta
+//llegar ala funcion .json si no la encuentra ...
+//lo de abajo lo que hace es parsearlo y pasarlo a objeto
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000
 
 app.get("/healthy", (req, res) => {
@@ -20,9 +25,9 @@ app.get("/roles",getRoles)
 
 app.post("/roles",createdRoles)
 
-app.put("/roles",updatedRoles)
+app.put("/roles/:id",updatedRoles)
 
-app.delete("/roles",deletedRoles)
+app.delete("/roles/:id",deletedRoles)
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT: ${PORT}`)
